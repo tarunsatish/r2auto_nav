8ac530e4dc9eb51aada58d290114c817be2994dd
sudowan <77694270+sudowan@users.noreply.github.com>
2021-04-21 10:03:05 +0800

    comment out some parts unused

--- mapping.py
+++ mapping.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import rclpy
-import cv2
+# import cv2
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
@@ -273,7 +273,7 @@ class AutoNav(Node):
             # find direction with the largest distance from the Lidar,
             # rotate to that direction, and start moving
             # self.pick_direction()
-            flag=0
+            # flag=0
             # self.pick_direction()
             # self.pick_direction()
             # self.pick_direction()
@@ -344,62 +344,62 @@ class AutoNav(Node):
             # stop moving
             self.stopbot()
 
-    def closure(self, mapdata):
-        # This function checks if mapdata contains a closed contour. The function
-        # assumes that the raw map data from SLAM has been modified so that
-        # -1 (unmapped) is now 0, and 0 (unoccupied) is now 1, and the occupied
-        # values go from 1 to 101.
-        # msgdata = np.array(msg.data)+1
-        # According to: https://stackoverflow.com/questions/17479606/detect-closed-contours?rq=1
-        # closed contours have larger areas than arc length, while open contours have larger
-        # arc length than area. But in my experience, open contours can have areas larger than
-        # the arc length, but closed contours tend to have areas much larger than the arc length
-        # So, we will check for contour closure by checking if any of the contours
-        # have areas that are more than 10 times larger than the arc length
-        # This value may need to be adjusted with more testing.
-        ALTHRESH = 10
-        # We will slightly fill in the contours to make them easier to detect
-        DILATE_PIXELS = 3
+#     def closure(self, mapdata):
+#         # This function checks if mapdata contains a closed contour. The function
+#         # assumes that the raw map data from SLAM has been modified so that
+#         # -1 (unmapped) is now 0, and 0 (unoccupied) is now 1, and the occupied
+#         # values go from 1 to 101.
+#         # msgdata = np.array(msg.data)+1
+#         # According to: https://stackoverflow.com/questions/17479606/detect-closed-contours?rq=1
+#         # closed contours have larger areas than arc length, while open contours have larger
+#         # arc length than area. But in my experience, open contours can have areas larger than
+#         # the arc length, but closed contours tend to have areas much larger than the arc length
+#         # So, we will check for contour closure by checking if any of the contours
+#         # have areas that are more than 10 times larger than the arc length
+#         # This value may need to be adjusted with more testing.
+#         ALTHRESH = 10
+#         # We will slightly fill in the contours to make them easier to detect
+#         DILATE_PIXELS = 3
 
-        # assumes mapdata is uint8 and consists of 0 (unmapped), 1 (unoccupied),
-        # and other positive values up to 101 (occupied)
-        # so we will apply a threshold of 2 to create a binary image with the
-        # occupied pixels set to 255 and everything else is set to 0
-        # we will use OpenCV's threshold function for this
-        ret,img2 = cv2.threshold(mapdata,2,255,0)
-        # we will perform some erosion and dilation to fill out the contours a
-        # little bit
-        element = cv2.getStructuringElement(cv2.MORPH_CROSS,(DILATE_PIXELS,DILATE_PIXELS))
-        img3 = cv2.erode(img2,element)
-        img4 = cv2.dilate(img2,element)
-        # use OpenCV's findContours function to identify contours
-        # OpenCV version 3 changed the number of return arguments, so we
-        # need to check the version of OpenCV installed so we know which argument
-        # to grab
-        fc = cv2.findContours(img4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        (major, minor, _) = cv2.__version__.split(".")
-        if(major == '3'):
-            contours = fc[1]
-        else:
-            contours = fc[0]
-        # find number of contours returned
-        lc = len(contours)
-        # rospy.loginfo('# Contours: %s', str(lc))
-        # create array to compute ratio of area to arc length
-        cAL = np.zeros((lc,2))
-        for i in range(lc):
-            cAL[i,0] = cv2.contourArea(contours[i])
-            cAL[i,1] = cv2.arcLength(contours[i], True)
+#         # assumes mapdata is uint8 and consists of 0 (unmapped), 1 (unoccupied),
+#         # and other positive values up to 101 (occupied)
+#         # so we will apply a threshold of 2 to create a binary image with the
+#         # occupied pixels set to 255 and everything else is set to 0
+#         # we will use OpenCV's threshold function for this
+#         ret,img2 = cv2.threshold(mapdata,2,255,0)
+#         # we will perform some erosion and dilation to fill out the contours a
+#         # little bit
+#         element = cv2.getStructuringElement(cv2.MORPH_CROSS,(DILATE_PIXELS,DILATE_PIXELS))
+#         img3 = cv2.erode(img2,element)
+#         img4 = cv2.dilate(img2,element)
+#         # use OpenCV's findContours function to identify contours
+#         # OpenCV version 3 changed the number of return arguments, so we
+#         # need to check the version of OpenCV installed so we know which argument
+#         # to grab
+#         fc = cv2.findContours(img4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#         (major, minor, _) = cv2.__version__.split(".")
+#         if(major == '3'):
+#             contours = fc[1]
+#         else:
+#             contours = fc[0]
+#         # find number of contours returned
+#         lc = len(contours)
+#         # rospy.loginfo('# Contours: %s', str(lc))
+#         # create array to compute ratio of area to arc length
+#         cAL = np.zeros((lc,2))
+#         for i in range(lc):
+#             cAL[i,0] = cv2.contourArea(contours[i])
+#             cAL[i,1] = cv2.arcLength(contours[i], True)
 
-        # closed contours tend to have a much higher area to arc length ratio,
-        # so if there are no contours with high ratios, we can safely say
-        # there are no closed contours
-        cALratio = cAL[:,0]/cAL[:,1]
-        # rospy.loginfo('Closure: %s', str(cALratio))
-        if np.any(cALratio > ALTHRESH):
-            return True
-        else:
-            return False
+#         # closed contours tend to have a much higher area to arc length ratio,
+#         # so if there are no contours with high ratios, we can safely say
+#         # there are no closed contours
+#         cALratio = cAL[:,0]/cAL[:,1]
+#         # rospy.loginfo('Closure: %s', str(cALratio))
+#         if np.any(cALratio > ALTHRESH):
+#             return True
+#         else:
+#             return False
 
 def main(args=None):
     rclpy.init(args=args)
